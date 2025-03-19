68c15e6d9d994cb2ac4d7592aafe4565cb839fb1
Michal Migurski <mmigurski@carto.com>
2025-03-18 18:01:30 -0700

    Getting current branch and history info

--- stack.py
+++ stack.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python3
+import subprocess
 import sys
 
 import networkx
 
-def main(argv):
-    print(argv)
+
+def get_main_sha() -> str:
+    output = subprocess.check_output(("git", "rev-parse", "main"))
+    return output.decode("utf8").strip()
+
+
+def get_curr_branch() -> str:
+    output = subprocess.check_output(("git", "rev-parse", "--abbrev-ref", "HEAD"))
+    return output.decode("utf8").strip()
+
+
+def get_sha_list() -> list[str]:
+    output = subprocess.check_output(("git", "rev-list", "HEAD"))
+    lines = output.decode("utf8").split()
+    return lines
+
+
+def main(action, args):
+    print(
+        action,
+        "branch:",
+        get_curr_branch(),
+        "main:",
+        get_main_sha()[:9],
+        "list:",
+        [s[:9] for s in get_sha_list()],
+        "args:",
+        args,
+    )
+
 
 if __name__ == "__main__":
-    exit(main(sys.argv))
+    action, *args = sys.argv[1:]
+    exit(main(action, args))
