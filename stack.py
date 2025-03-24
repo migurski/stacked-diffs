6a234ab3e107e24a3d6bef99a7e30c8527f1aaf7
Michal Migurski <mmigurski@carto.com>
2025-03-23 19:36:58 -0700

    Renamed "curr" to "head"

--- stack.py
+++ stack.py
@@ -40,7 +40,7 @@ def get_main_branch() -> tuple[str, str]:
 
 
 @functools.cache
-def get_curr_branch() -> tuple[str, str]:
+def get_head_branch() -> tuple[str, str]:
     branchname = get_output(("git", "rev-parse", "--abbrev-ref", "HEAD")).strip()
     sha = get_output(("git", "rev-parse", "HEAD")).strip()
     return branchname, sha
@@ -60,7 +60,7 @@ def read_graph():
     else:
         graph = networkx.DiGraph()
     main_branch, main_sha = get_main_branch()
-    curr_branch, curr_sha = get_curr_branch()
+    head_branch, head_sha = get_head_branch()
     graph.add_node(main_branch, sha=main_sha)
 
     yield graph
@@ -68,83 +68,85 @@ def read_graph():
     for line in networkx.generate_network_text(graph):
         node_id = line.split(" ")[-1].strip()
         node_sha = graph.nodes[node_id]["sha"]
-        print("==>" if node_sha == curr_sha else "   ", line, node_sha[:7])
+        print("==>" if node_sha == head_sha else "   ", line, node_sha[:7])
     with open(".stack.json", "w") as file:
         json.dump(networkx.node_link_data(graph, edges="edges"), file, indent=2)
 
 
-def update_branch(graph: networkx.DiGraph, curr_branch: str, curr_sha: str):
-    graph.nodes[curr_branch]["sha"] = curr_sha
-    for parent_branch in graph.predecessors(curr_branch):
-        base_sha = get_output(("git", "merge-base", parent_branch, curr_branch)).strip()
-        graph.nodes[curr_branch]["base"] = base_sha
+def update_branch(graph: networkx.DiGraph, head_branch: str, head_sha: str):
+    graph.nodes[head_branch]["sha"] = head_sha
+    for parent_branch in graph.predecessors(head_branch):
+        base_sha = get_output(("git", "merge-base", parent_branch, head_branch)).strip()
+        graph.nodes[head_branch]["base"] = base_sha
 
 
-def restack_branch(graph: networkx.DiGraph, curr_branch: str, curr_sha: str):
-    if graph.nodes[curr_branch]["sha"] != curr_sha:
+def restack_branch(graph: networkx.DiGraph, head_branch: str, head_sha: str):
+    if graph.nodes[head_branch]["sha"] != head_sha:
         raise ValueError("Current branch SHA incorrect")
-    (parent_branch,) = graph.predecessors(curr_branch)
+    (parent_branch,) = graph.predecessors(head_branch)
     new_base_sha = graph.nodes[parent_branch]["sha"]
     run_command(("git", "rebase", new_base_sha))
-    graph.nodes[curr_branch]["base"] = new_base_sha
+    graph.nodes[head_branch]["base"] = new_base_sha
 
 
 def move_branch(
-    graph: networkx.DiGraph, curr_branch: str, curr_sha: str, new_parent: str
+    graph: networkx.DiGraph, head_branch: str, head_sha: str, new_parent: str
 ):
-    if graph.nodes[curr_branch]["sha"] != curr_sha:
+    if graph.nodes[head_branch]["sha"] != head_sha:
         raise ValueError("Current branch SHA incorrect")
-    (parent_branch,) = graph.predecessors(curr_branch)
+    (parent_branch,) = graph.predecessors(head_branch)
     if new_parent == parent_branch:
         # No-op
         return
-    logging.info("Move %s %s onto %s", curr_branch, curr_sha[:9], new_parent)
+    logging.info("Move %s %s onto %s", head_branch, head_sha[:9], new_parent)
     new_base_sha = graph.nodes[new_parent]["sha"]
     run_command(("git", "rebase", new_base_sha))
-    graph.nodes[curr_branch]["base"] = new_base_sha
-    graph.remove_edge(parent_branch, curr_branch)
-    graph.add_edge(new_parent, curr_branch)
+    graph.nodes[head_branch]["base"] = new_base_sha
+    graph.remove_edge(parent_branch, head_branch)
+    graph.add_edge(new_parent, head_branch)
 
 
 def add_branch(
-    graph: networkx.DiGraph, parent_sha: str, curr_branch: str, curr_sha: str
+    graph: networkx.DiGraph, parent_sha: str, head_branch: str, head_sha: str
 ):
     graph_shas = {graph.nodes[node_id]["sha"]: node_id for node_id in graph.nodes}
     logging.info(
         "Add %s %s %s",
-        curr_branch,
-        curr_sha[:9],
+        head_branch,
+        head_sha[:9],
         {s[:9]: b for s, b in graph_shas.items()},
     )
     for other_sha in get_sha_list():
-        if other_sha == curr_sha:
-            graph.add_node(curr_branch, sha=curr_sha, base=other_sha)
-            graph.add_edge(graph_shas[parent_sha], curr_branch)
+        if other_sha == head_sha:
+            graph.add_node(head_branch, sha=head_sha, base=other_sha)
+            graph.add_edge(graph_shas[parent_sha], head_branch)
             break
 
 
 def main(action, args):
     with read_graph() as graph:
-        curr_branch, curr_sha = get_curr_branch()
-        if curr_branch == "HEAD":
+        head_branch, head_sha = get_head_branch()
+        if head_branch == "HEAD":
             return
-        if curr_branch in graph.nodes:
-            update_branch(graph, curr_branch, curr_sha)
+        if head_branch in graph.nodes:
+            update_branch(graph, head_branch, head_sha)
         if action == Actions.restack:
-            if curr_branch in graph.nodes:
-                restack_branch(graph, curr_branch, curr_sha)
+            if head_branch not in graph.nodes:
+                raise ValueError(f"Unknown branch {head_branch}")
             else:
-                raise NotImplementedError()
+                restack_branch(graph, head_branch, head_sha)
         elif action == Actions.move_onto:
             (new_parent,) = args
-            if new_parent in graph.nodes:
-                move_branch(graph, curr_branch, curr_sha, new_parent)
+            if new_parent not in graph.nodes:
+                raise ValueError(f"Unknown branch {new_parent}")
+            elif head_branch not in graph.nodes:
+                raise ValueError(f"Unknown branch {head_branch}")
             else:
-                raise NotImplementedError()
-        elif action == Actions.post_checkout and curr_branch not in graph.nodes:
+                move_branch(graph, head_branch, head_sha, new_parent)
+        elif action == Actions.post_checkout and head_branch not in graph.nodes:
             parent_sha, is_branch = args
             if is_branch == "1":
-                add_branch(graph, parent_sha, curr_branch, curr_sha)
+                add_branch(graph, parent_sha, head_branch, head_sha)
 
 
 parser = argparse.ArgumentParser()
